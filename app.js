4d13789d07cbc386bdce270b8fc2c7a92dd69f1d
PayyavulaRajasekhar <payyavularajasekhar@gmail.com>
2021-06-22 10:56:59 +0530

    Initial commit

--- app.js
+++ app.js
@@ -3,16 +3,31 @@ const cors = require('cors');
 const app = express();
 const port = 2000;
 
+// env config
+require('dotenv').config();
+
+// importing database
+require('./config/db');
+
+// cors middleware
 app.use(cors());
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+// bodyparser middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({
-        request: req.headers,
+        requestHeaders: req.headers,
+        responseHeaders: res.getHeaders(),
         app: 'Medifit',
         path: '/'
+    });
+});
+
+app.use((err, req, res, next) => {
+    res.json({
+        message: 'Some Error Occurred'
     });
 });
 

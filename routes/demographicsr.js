d33eae0d6cd1a280943cb29fa4748e2cf522f515
TE_Sanjay_Kumar <sanjay.s@testyantra.in>
2021-08-27 11:13:59 +0530

    updated swagger.json

--- routes/demographicsr.js
+++ routes/demographicsr.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middlewares/auth');
 const multer = require('multer');
-const { Personal } = require('../models/demographicsm');
-const { Goal } = require('../models/demographicsm')
+const { Personal,Goal } = require('../models/demographicsm');
+const { BodyFitness,Image } = require('../models/bodyprofilem');
 const {goals, putGoals ,userDetails,updateDetails} = require('../controllers/demographics-controllers')
 const {imageController, bodyFitness, putBodyFitness} = require('../controllers/bodyprofile-controllers');
 
 
-router.get('/details',auth, async (req, res) => {
+router.get('/personalinfo',auth, async (req, res) => {
     const persons = await Personal.find();
     res.send(persons);
 })
@@ -18,21 +18,24 @@ router.get('/goals',auth, async (req, res) => {
     res.send(goals);
 })
 
-router.get('/images', imageController.getAllImages)
+router.get('/images', async (req, res) => {
+    const images = await Image.find();
+    res.send(images); 
+});
 
 router.get('/bodyfitness', async (req, res) => {
     const bodyFitness = await BodyFitness.find();
     res.send(bodyFitness);
 })
 
-router.post('/details',auth,userDetails)
-router.put('/details/:id',auth,updateDetails)
-router.post('/goals',auth, goals)
-router.put('/putgoals/:id',auth, putGoals)
-router.post('/images', auth, multer({ storage: imageController.storage }).fields([{ name: 'front', maxCount: 1 }, { name: 'back', maxCount: 1 }, { name: 'side', maxCount: 1 }]), imageController.postImage);
-router.put('/images/:id',auth, multer({ storage: imageController.storage }).fields([{ name: 'front', maxCount: 1 }, { name: 'back', maxCount: 1 }, { name: 'side', maxCount: 1 }]), imageController.updateImage);
-router.post('/bodyfitness',auth, bodyFitness)
-router.put('/putbodyfitness/:id',auth, putBodyFitness)
+router.post('/add-personalinfo',auth,userDetails)
+router.put('/put-personalinfo/:id',auth,updateDetails)
+router.post('/add-goals',auth, goals)
+router.put('/put-goals/:id',auth, putGoals)
+router.post('/add-images', auth, multer({ storage: imageController.storage }).fields([{ name: 'front', maxCount: 1 }, { name: 'back', maxCount: 1 }, { name: 'side', maxCount: 1 }]), imageController.postImage);
+router.put('/put-images/:id',auth, multer({ storage: imageController.storage }).fields([{ name: 'front', maxCount: 1 }, { name: 'back', maxCount: 1 }, { name: 'side', maxCount: 1 }]), imageController.updateImage);
+router.post('/add-bodyfitness',auth, bodyFitness)
+router.put('/put-bodyfitness/:id',auth, putBodyFitness)
 
 
 module.exports = router;
